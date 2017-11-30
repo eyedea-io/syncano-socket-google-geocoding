9b17cc30a9cc6304e3c5d26902c6d32735adb417
Kasper Mikiewicz <IderedPL@gmail.com>
2017-11-30 17:06:33 +0100

    Update get-sublocality.js

--- src/get-sublocality.js
+++ src/get-sublocality.js
@@ -9,7 +9,7 @@ export default async (ctx) => {
     const resp = await axios.get(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${ctx.config.API_KEY}`
     )
-    response.json({sublocality: resp.data.results[0].address_components[3].short_name})
+    response.json({sublocality: resp.data.results[0].address_components[2].short_name})
   } catch (err) {
     response.json({ message: err.message }, 400)
   }
